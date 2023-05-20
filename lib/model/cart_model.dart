b4e2b6742a92d2cb91c5b0cccc18bf771c71fbcd
Ömer Yusuf Akyol <omeryusufakyol2003@gmail.com>
2023-05-20 22:04:19 +0300

    project
    project

---
+++ lib/model/cart_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CartModel extends ChangeNotifier {
+  // list of items on sale
+  final List _shopItems = const [
+    // [ itemName, itemPrice, imagePath, color ]
+    ["Iphone XS", "400.00", "lib/images/ixs.png", Colors.green],
+    ["Samsung A53", "450.50", "lib/images/sa53.png", Colors.yellow],
+    ["Huawei P30 Pro", "375.80", "lib/images/hp30.png", Colors.brown],
+    ["Oppo Find X5", "350.00", "lib/images/ofx5.png", Colors.blue],
+  ];
+
+  // list of cart items
+  List _cartItems = [];
+
+  get cartItems => _cartItems;
+
+  get shopItems => _shopItems;
+
+  // add item to cart
+  void addItemToCart(int index) {
+    _cartItems.add(_shopItems[index]);
+    notifyListeners();
+  }
+
+  // remove item from cart
+  void removeItemFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  // calculate total price
+  String calculateTotal() {
+    double totalPrice = 0;
+    for (int i = 0; i < cartItems.length; i++) {
+      totalPrice += double.parse(cartItems[i][1]);
+    }
+    return totalPrice.toStringAsFixed(2);
+  }
+}
